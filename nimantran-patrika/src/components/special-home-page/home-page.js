5012fd8be268e5d77e2d31d8879262902d00d6a9
Ketan Sunil Kulkarni <kketan482@gmail.com>
2024-11-10 18:49:20 +0530

    background-of-home-page-setup-done

--- nimantran-patrika/src/components/special-home-page/home-page.js
+++ nimantran-patrika/src/components/special-home-page/home-page.js
@@ -1,10 +1,31 @@
-const HomePage=()=>{
+import { useRef, useEffect } from "react";
+import "./home-page-style.css";
 
-    return(
-    <div>
-        <>Test</>
-    </div>)
- 
-}
+import backgroundVideo from "../../assets/videos/videoplayback.mp4"; // Import your video here
 
-export default HomePage
+const HomePage = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 0.6; // Adjust this value to slow down the speed (e.g., 0.5 is half speed)
+        }
+    }, []);
+
+    return (
+        <div className="container">
+            {/* Background video */}
+            <video
+                className="background-video"
+                src={backgroundVideo}
+                autoPlay
+                loop
+                muted
+                ref={videoRef} // Attach ref to the video element
+            ></video>
+
+        </div>
+    );
+};
+
+export default HomePage;
